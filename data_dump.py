c570bd133bebba118d97646b450db30eeb0b7a1a
Nitha-SKumar <nithaskumar@gmail.com>
2023-10-23 19:51:44 +0400

    data pushed over database

--- data_dump.py
+++ data_dump.py
@@ -1,22 +1,32 @@
 import pandas as pd
 import pymongo
 import json
-#from pymongo.mongo_client import MongoClient
 
-client = "mongodb+srv://Nitha:Shivbaba@cluster0.6tmpzbq.mongodb.net/?retryWrites=true&w=majority"
+# MongoDB connection URI
+uri = "mongodb+srv://Nitha:nitha123@cluster0.6tmpzbq.mongodb.net/?retryWrites=true&w=majority"
 
-DATA_FILE_PATH = (r"/Users/nitha/Downloads/ML Bootcamp/Machine_learning-Application/src/Data/train.csv")
-DATABASE ="MAchine_learning"
-COLLECTION_NAME ="DATASET"
-if __name__ =="__main__":
-    df=pd.read_csv(DATA_FILE_PATH)
-    print(f"Rows and columns of our data: {df.shape}")
-    
-    df.reset_index(drop =True, inplace =True)
-    joson_record =list(json.loads(df.T.to_json()).values())
-    
-    print(joson_record[0])
-    
-    client[DATABASE][COLLECTION_NAME].insert_many(joson_record)
-    
-    
+DATA_FILE_PATH = r"/Users/nitha/Downloads/ML Bootcamp/Data/train.csv"
+DATABASE = "Machine_learning"
+COLLECTION_NAME = "DATASET"
+
+if __name__ == "__main__":
+    # Read data from the CSV file into a Pandas DataFrame
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    # Convert the DataFrame to a list of dictionaries (JSON records)
+    json_records = json.loads(df.to_json(orient="records"))
+    print(json_records[0])
+
+    # Establish a connection to MongoDB
+    client = pymongo.MongoClient(uri)
+
+    # Access the desired database and collection
+    db = client[DATABASE]
+    collection = db[COLLECTION_NAME]
+
+    # Insert the JSON records into the collection
+    collection.insert_many(json_records)
+
+    # Close the MongoDB connection
+    client.close()
